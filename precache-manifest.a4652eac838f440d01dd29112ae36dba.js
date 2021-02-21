692122fbf2fb5d9ed56286ae1ecebbdf834368a9
Joseph <josephtsegen10@gmail.com>
2021-02-21 15:03:44 +0100

    Updates

---
+++ precache-manifest.a4652eac838f440d01dd29112ae36dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0463e5ef91da5bfc3747",
+    "url": "/covid-19-tracker/static/css/main.fa6989c4.chunk.css"
+  },
+  {
+    "revision": "0463e5ef91da5bfc3747",
+    "url": "/covid-19-tracker/static/js/main.8be1325b.chunk.js"
+  },
+  {
+    "revision": "97e120a5131d402c344f",
+    "url": "/covid-19-tracker/static/js/runtime~main.6269f788.js"
+  },
+  {
+    "revision": "bf5a5193fec067d5dee3",
+    "url": "/covid-19-tracker/static/css/2.cdbef9f4.chunk.css"
+  },
+  {
+    "revision": "bf5a5193fec067d5dee3",
+    "url": "/covid-19-tracker/static/js/2.ecb4d3d5.chunk.js"
+  },
+  {
+    "revision": "3e0e739664eebbc5f279c78ab38e4739",
+    "url": "/covid-19-tracker/index.html"
+  }
+];
